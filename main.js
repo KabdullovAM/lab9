1151e71f5185d0a23510886e4422b5c5ac3aeb18
Aldiyar <ualdiyar@gmail.com>
2022-10-23 14:28:37 +0600

    lab9

---
+++ main.js
@@ -0,0 +1,52 @@
+let a='question2'
+let b=question2(2, 3)
+function question2(c, d)
+{
+	Perimeter=c*2+d*2
+	const result2=
+	{
+		a,
+		Perimeter
+	}
+	return result2
+}
+console.log(b)
+
+let e='question3'
+let f=question3(2, 3.14)
+function question3(r, PI)
+{
+	Area=PI*r*r
+	const result3=
+	{
+		e,
+		Area
+	}
+	return result3
+}
+console.log(f)
+
+let g='question9'
+let j=question9(-10)
+function question9(h)
+{
+	if (h>0)
+	{
+		i='positive'
+	}
+	else if(h<0)
+	{
+		i='negative'
+	}
+	else
+	{
+		i='null'
+	}
+	const result9=
+	{
+		g,
+		i
+	}
+	return result9
+}
+console.log(j)
